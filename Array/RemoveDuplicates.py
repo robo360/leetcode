ccc3c972c72d1b01381a9166a1927694d38b0df7
robo360 <berwa05@gmail.com>
2020-07-31 22:22:56 -0700

    reverseString

--- Array/RemoveDuplicates.py
+++ Array/RemoveDuplicates.py
@@ -2,6 +2,8 @@
 Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
 
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The solution is based on two-finger swapping.
 """
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
---
+++ Strings /reverseString_easy.py
@@ -0,0 +1,18 @@
+"""
+Write a function that reverses a string. The input string is given as an array of characters char[].
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+You may assume all the characters consist of printable ascii characters.
+"""
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        i = 0
+        j = len(s) - 1
+        while j - i > 0:
+            temp = s[i]
+            s[i] = s[j]
+            s[j] = temp
+            j -= 1
+            i += 1
