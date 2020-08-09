6b29c3e3c36affcc68efe5f9b1c82f2eebae813c
robo360 <berwa05@gmail.com>
2020-08-08 23:50:35 -0400

    deleteNum

---
+++ Array/deleteNum.py
@@ -0,0 +1,16 @@
+"""
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+"""
+class Solution:
+    def removeElement(self, nums, val):
+        i = 0 
+        while i < len(nums):
+            if(nums[i] == val):
+                del nums[i]
+            else:
+                i += 1 
+        return len(nums)
